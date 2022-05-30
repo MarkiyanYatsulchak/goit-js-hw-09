84601fce528ea612f8740059d39c57144180e689
MarkiyanYatsulchak <m.yatsulchak@gmail.com>
2022-05-30 14:02:35 +0300

    +

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,15 @@
 import flatpickr from 'flatpickr';
-
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  input: document.querySelector('#datetime-picker'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
 
 const options = {
   enableTime: true,
@@ -8,82 +17,94 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0].getTime() <= options.defaultDate.getTime()) {
-      Notify.failure('Please choose a future date');
-      return;
+    easyTimer.calculateCountdownTime(selectedDates);
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+class CountdownTimer {
+  constructor({ updateOnTick, startBtnIsDisabled, inputIsDisabled }) {
+    this.countdownTime = 0;
+    this.countdownID = null;
+    this.updateOnTick = updateOnTick;
+    this.startBtnIsDisabled = startBtnIsDisabled;
+    this.inputIsDisabled = inputIsDisabled;
+  }
+
+  calculateCountdownTime(selectedDates) {
+    const currentTime = Date.now();
+    const selectedTime = selectedDates[0].getTime();
+
+    if (selectedTime < currentTime) {
+      this.startBtnIsDisabled(true);
+      return Notify.failure('Please choose a date in the future');
     }
-    //   selectedDates = selectedDates[0];
-  },
-};
 
-const inputDate = document.querySelector('#datetime-picker');
-const btnStart = document.querySelector('[data-start]');
-const btnStop = document.querySelector('[data-stop]');
-const days = document.querySelector('[data-days]');
-const hours = document.querySelector('[data-hours]');
-const minutes = document.querySelector('[data-minutes]');
-const seconds = document.querySelector('[data-seconds]');
+    this.countdownTime = selectedTime - currentTime;
+    this.startBtnIsDisabled(false);
+  }
 
-const timer = {
-  isActive: false,
-  start() {
-    const startTime = Date.now();
+  countdownStart() {
+    this.countdownID = setInterval(() => {
+      if (this.countdownTime < 0) {
+        return clearInterval(this.countdownID);
+      }
 
-    setInterval(() => {
-      const currentTime = Date.now();
+      const time = this.convertMs(this.countdownTime);
+      this.updateOnTick(time);
+      this.countdownTime -= 1000;
     }, 1000);
-    const deltaTime = startTime - currentTime;
-    const { days, hours, minutes, seconds } = convertMs(deltaTime);
-  },
-};
 
-timer.start();
-// class Timer {
-//     constructor() {
-//         this.countdownTime = 0;
-//         this.timerId = null;
-//     }
+    this.startBtnIsDisabled(true);
+    this.inputIsDisabled(true);
+  }
 
-//     updateCountdown({ days, hours, minutes, seconds }) {
-//         days.textContent = days;
-//         hours.textContent = hours;
-//         minutes.textContent = minutes;
-//         seconds.textContent = seconds;
-//     }
+  convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
-//     btnStartStatus(status) {
-//         btnStart.disabled = status;
-//     }
+    const days = this.addLeadingZero(Math.floor(ms / day));
+    const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
+    const minutes = this.addLeadingZero(
+      Math.floor(((ms % day) % hour) / minute)
+    );
+    const seconds = this.addLeadingZero(
+      Math.floor((((ms % day) % hour) % minute) / second)
+    );
 
-//     inputDateStatus(status) {
-//         inputDate.disabled = status;
-//     }
+    return { days, hours, minutes, seconds };
+  }
 
-//     btnStopStatus(status) {
-//         btnStop.disabled = status;
-//     }
-
-// }
-
-function pad(value) {
-  return String(value).padStart(2, '0');
+  addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  }
 }
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+const easyTimer = new CountdownTimer({
+  updateOnTick: updateCountdownEasyTimer,
+  startBtnIsDisabled: startBtnIsDisabledEasyTimer,
+  inputIsDisabled: inputIsDisabledEasyTimer,
+});
 
-  // Remaining days
-  const days = pad(Math.floor(ms / day));
-  // Remaining hours
-  const hours = pad(Math.floor((ms % day) / hour));
-  // Remaining minutes
-  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
-  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+refs.startBtn.disabled = true;
+refs.input.disabled = false;
 
-  return { days, hours, minutes, seconds };
+refs.startBtn.addEventListener('click', () => easyTimer.countdownStart());
+
+function updateCountdownEasyTimer({ days, hours, minutes, seconds }) {
+  refs.days.textContent = days;
+  refs.hours.textContent = hours;
+  refs.minutes.textContent = minutes;
+  refs.seconds.textContent = seconds;
+}
+
+function startBtnIsDisabledEasyTimer(state) {
+  refs.startBtn.disabled = state;
+}
+
+function inputIsDisabledEasyTimer(state) {
+  refs.input.disabled = state;
 }
